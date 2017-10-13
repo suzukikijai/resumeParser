bb0fb6261245e45f2fb5580538fadc6ffb07f97d
Sooraj Antony <soorajantony@live.in>
2017-10-13 16:31:12 +0530

    File input interface

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+//AUTHOR: suzukikijai22
+?>
+
+
+<html>
+   <body>
+      
+      <form action="transcriber.php" method="POST" enctype="multipart/form-data">
+         <input type="file" name="resume"/>
+         <input type="submit"/>
+      </form>
+      
+   </body>
+</html>
+
+
+
+
